7fc3b75a81f64b5956f967bd029a5faf2e0b491d
gregoriovelasquezgomez <g.velasquez@sonnen.de>
2023-11-24 15:35:46 +0100

    mejor manejo del input data para la grafica ejemplo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-@author: gregoriovelasquezgomez
+@author: gregoriovelasquezgomez, StevenJG
 """
 # librerias
 from flask import Flask, render_template
@@ -12,18 +12,6 @@ import numpy as np
 import matplotlib.cm as cm
 from matplotlib import rcParams
 
-# Configuración de entrada
-start_date = "2023-11-01"
-end_date = "2023-11-17"
-dataset_id_SiMEM= "2bff14"
-
-# Conexión a la información de SiMEM
-url = "https://www.simem.co/backend-files/api/PublicData?startdate=" + start_date + "&enddate=" + end_date + "&datasetId=" + dataset_id_SiMEM
-response = requests.get(url)
-
-# Organización de información en una tabla Pandas
-data = pd.read_json(response.text)
-aportes_hidr = pd.json_normalize(data.loc['records']['result'])
 
 
 app = Flask(__name__)
@@ -32,16 +20,42 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/datos_SiMEM", methods=["GET", "POST"])
+def datos_SiMEM():
+    # Configuración de entrada
+    start_date = "2023-11-15"
+    end_date = "2023-11-17"
+    dataset_id_SiMEM= "2bff14" #id aportes hídricos
 
-@app.route('/Random_Data', methods=["GET", "POST"])
-def Random_Data():
-    Random_Data = []
-    for i in range(1,10):
-        Random_Data.append(random.randint(0,100))
-    my_data = {
-        "YData":Random_Data
+    # Conexión a la información de SiMEM
+    url = "https://www.simem.co/backend-files/api/PublicData?startdate=" + start_date + "&enddate=" + end_date + "&datasetId=" + dataset_id_SiMEM
+    response = requests.get(url)
+
+    # Organización de información en una tabla Pandas
+    data = pd.read_json(response.text)
+    aportes_hidr = pd.json_normalize(data.loc['records']['result'])
+ 
+    return aportes_hidr.head().to_dict()
+
+@app.route('/Example_Data', methods=["GET", "POST"])
+def Example_Data():
+    size_data = 10
+    Example_Data = []
+    for i in range(1,size_data+1):
+        Example_Data.append(random.randint(0,100))
+
+    y_data = {
+        "YData":Example_Data
     }
-    return my_data
+
+    lineal_x_axis = []
+    for i in range(0,size_data):
+        lineal_x_axis.append(i)
+
+    x_data = {
+        "XData":lineal_x_axis
+    }
+    return y_data
 
 if __name__ == "__main__":
     app.run(debug=True)
